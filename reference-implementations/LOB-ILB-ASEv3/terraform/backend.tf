6e037ddff7a457b681914a3f94955b3865dd905c
Brent McConnell <brmcconn@microsoft.com>
2022-09-20 14:20:27 -0400

    Update backend.tf

--- reference-implementations/LOB-ILB-ASEv3/terraform/backend.tf
+++ reference-implementations/LOB-ILB-ASEv3/terraform/backend.tf
@@ -6,8 +6,8 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "backend-appsrvc-dev-westus2-001"
-    storage_account_name = "stbackendappsrwestus2001"
+    resource_group_name  = "backend-ebm-appsrvc-dev-eastus2-001"
+    storage_account_name = "stbackendebmaeastus2001"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
